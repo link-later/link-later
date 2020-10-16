a4ebe7640556aaf79108f454b4ff0d1e7ea860a6
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-16 12:33:26 -0400

    context provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import UserContext from './Context.js';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <UserContext.Provider value={'steve'}>
     <App />
-  </React.StrictMode>,
+  </UserContext.Provider>,
   document.getElementById('root')
 );
 
