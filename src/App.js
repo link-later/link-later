54bfdc981854f0902a7ffc3a17ae7e36405ae0c0
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-18 14:12:04 -0400

    added styled components and renders HOC for login/signup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { useContext } from 'react';
-import './App.css';
 // import { Switch, Route, Link } from 'react-router-dom';
 import UserContext from './Context.js';
-import Login from './Login.js';
+import styled from 'styled-components';
+import Login from './Login';
 
+const Form = styled.form`
+  flex: 1;
+  padding: 8px;
+`;
+const Input = styled.input`
+  margin: 4px;
+`;
 function App() {
+  const context = useContext(UserContext);
+
   return (
-    <UserContext.Consumer>
-      <div className='App'>
-        Hi {context}, Welcome to Link Later
-        <Login />
-      </div>
-    </UserContext.Consumer>
+    <div className='App'>
+      Hi {context}, Welcome to Link Later
+      <Login>
+        Login!
+        <Form>
+          Username
+          <Input text='Username'></Input>
+        </Form>
+        <Form>
+          Password
+          <Input text='Password'></Input>
+        </Form>
+      </Login>
+      <Login>
+        Sign up!
+        <Form>
+          Email
+          <Input text='Email'></Input>
+        </Form>
+        <Form>
+          Username
+          <Input text='Username'></Input>
+        </Form>
+        <Form>
+          Password
+          <Input text='Password'></Input>
+        </Form>
+      </Login>
+    </div>
   );
 }
 
