ece09bd98693d32784d191bb5d2fbd7987b4948a
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-18 14:14:55 -0400

    takes state hook and method to change status on click

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,27 @@
-import React, { useContext } from 'react';
-import UserContext from './Context.js';
+import React from 'react';
+import styled from 'styled-components';
+import WithClicker from './HOCs/WithClicker';
 
-const Login = () => {
-  const context = useContext(UserContext);
-  return <div>Login</div>;
+const Div = styled.div`
+  border-radius: 3px;
+  border: 2px solid firebrick;
+  padding: 12px;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Login = (props) => {
+  const { clicked, handleClick } = WithClicker();
+  console.log(handleClick);
+  return (
+    <Div>
+      {props.children}
+      <button onClick={handleClick}>Click</button>
+      {clicked ? 'clicked' : 'unclicked'}
+    </Div>
+  );
 };
 
 export default Login;
