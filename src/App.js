b539505765ef949fa472677cbb221cd4991eceb2
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-20 12:46:39 -0400

    refactored out of HOCs

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
 import React, { useContext } from 'react';
 // import { Switch, Route, Link } from 'react-router-dom';
 import UserContext from './Context.js';
-import styled from 'styled-components';
 import Login from './Login';
+import Signup from './Signup';
 
-const Form = styled.form`
-  flex: 1;
-  padding: 8px;
-`;
-const Input = styled.input`
-  margin: 4px;
-`;
 function App() {
   const context = useContext(UserContext);
 
   return (
     <div className='App'>
       Hi {context}, Welcome to Link Later
-      <Login>
-        Login!
-        <Form>
-          Username
-          <Input text='Username'></Input>
-        </Form>
-        <Form>
-          Password
-          <Input text='Password'></Input>
-        </Form>
-      </Login>
-      <Login>
-        Sign up!
-        <Form>
-          Email
-          <Input text='Email'></Input>
-        </Form>
-        <Form>
-          Username
-          <Input text='Username'></Input>
-        </Form>
-        <Form>
-          Password
-          <Input text='Password'></Input>
-        </Form>
-      </Login>
+      <Login />
+      <Signup />
     </div>
   );
 }
