758f7721ac54275904b764cf11e66d67205a00d6
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-20 12:47:17 -0400

    added useref hooks and handlers to export to login/signup

--- src/HOCs/WithClicker.js
+++ src/HOCs/WithClicker.js
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const WithClicker = (initial = false) => {
   const [clicked, setClicked] = useState(initial);
+  const user = useRef(null);
+  const pass = useRef(null);
+  const email = useRef(null);
 
-  const handleClick = () => {
-    setClicked(!clicked);
+  const handleClick = (e) => {
+    // e.preventDefault();
+    console.log('user', user.current.value);
+    console.log('pass', pass.current.value);
   };
 
-  return { clicked, handleClick };
+  return { clicked, handleClick, user, pass, email };
 };
 export default WithClicker;
