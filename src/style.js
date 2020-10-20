9ff478f1524b2f816bc95546c7c7717331ad0a88
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-20 12:48:31 -0400

    created global styled components for auth forms

---
+++ src/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  border-radius: 3px;
+  border: 2px solid firebrick;
+  padding: 12px;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Form = styled.form`
+  flex: 1;
+  padding: 8px;
+`;
+export const Input = styled.input`
+  margin: 4px;
+`;
