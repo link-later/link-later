d8c601bb165c423f7ad535e3e91c10ec27e43032
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-21 14:57:05 -0400

    change context on click

--- src/HOCs/WithClicker.js
+++ src/HOCs/WithClicker.js
@@ -1,17 +1,20 @@
-import { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import UserContext from '../Context';
 
 const WithClicker = (initial = false) => {
-  const [clicked, setClicked] = useState(initial);
+  const { checkUser, setCheckUser } = useContext(UserContext);
   const user = useRef(null);
   const pass = useRef(null);
   const email = useRef(null);
 
   const handleClick = (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     console.log('user', user.current.value);
     console.log('pass', pass.current.value);
+    setCheckUser(true);
   };
 
-  return { clicked, handleClick, user, pass, email };
+  return { checkUser, handleClick, user, pass, email };
 };
 export default WithClicker;
