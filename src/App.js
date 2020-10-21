b0c6a9b679bc62c87a00e624a6a0e3102e502095
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-21 14:56:10 -0400

    created routing and context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { useContext } from 'react';
-// import { Switch, Route, Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import UserContext from './Context.js';
 import Login from './Login';
 import Signup from './Signup';
+import WithClicker from './HOCs/WithClicker';
+import Links from './Links';
 
 function App() {
-  const context = useContext(UserContext);
-
+  const [checkUser, setCheckUser] = useState(false);
+  const value = { checkUser, setCheckUser };
+  console.log('check user', checkUser);
   return (
-    <div className='App'>
-      Hi {context}, Welcome to Link Later
-      <Login />
-      <Signup />
-    </div>
+    <UserContext.Provider value={value}>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => {
+            return checkUser ? (
+              <Redirect to='/links' />
+            ) : (
+              <Redirect to='/login' />
+            );
+          }}
+        ></Route>
+        <Route exact path='/login'>
+          <Login />
+          <Signup />
+        </Route>
+        <Login />
+        <Signup />
+        <Route exact path='/links'>
+          <Links />
+        </Route>
+      </Switch>
+    </UserContext.Provider>
   );
 }
 
