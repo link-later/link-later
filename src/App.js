bfcb0ef134ce3ae5b4e4f91b5b3a5a563d99c321
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-22 11:38:19 -0400

    added conditional routing and set context value to be state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import UserContext from './Context.js';
-import Login from './Login';
-import Signup from './Signup';
-import WithClicker from './HOCs/WithClicker';
 import Links from './Links';
+import Auth from './HOCs/Auth';
 
 function App() {
   const [checkUser, setCheckUser] = useState(false);
   const value = { checkUser, setCheckUser };
-  console.log('check user', checkUser);
   return (
     <UserContext.Provider value={value}>
       <Switch>
@@ -20,16 +17,17 @@ function App() {
             return checkUser ? (
               <Redirect to='/links' />
             ) : (
-              <Redirect to='/login' />
+              <Redirect to='/auth' />
             );
           }}
         ></Route>
-        <Route exact path='/login'>
-          <Login />
-          <Signup />
-        </Route>
-        <Login />
-        <Signup />
+        <Route
+          exact
+          path='/auth'
+          render={() => {
+            return checkUser ? <Redirect to='/links' /> : <Auth />;
+          }}
+        ></Route>
         <Route exact path='/links'>
           <Links />
         </Route>
